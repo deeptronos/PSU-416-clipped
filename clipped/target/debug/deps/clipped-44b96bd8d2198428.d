55b105a570a7681da3e6cf8d457333a7ff4d8fc3
C <colnem@pdx.edu>
2024-10-10 12:18:18 -0700

    Added dep for wav encoding/decoding library

--- clipped/target/debug/deps/clipped-44b96bd8d2198428.d
+++
@@ -1,5 +0,0 @@
-/Users/cole_sairbackup/Developer/PSU/CS416P/clipped/clipped/target/debug/deps/libclipped-44b96bd8d2198428.rmeta: src/main.rs
-
-/Users/cole_sairbackup/Developer/PSU/CS416P/clipped/clipped/target/debug/deps/clipped-44b96bd8d2198428.d: src/main.rs
-
-src/main.rs:
--- clipped/target/debug/deps/clipped-689ce660451a9e6f.d
+++
@@ -1,5 +0,0 @@
-/Users/cole_sairbackup/Developer/PSU/CS416P/clipped/clipped/target/debug/deps/libclipped-689ce660451a9e6f.rmeta: src/main.rs
-
-/Users/cole_sairbackup/Developer/PSU/CS416P/clipped/clipped/target/debug/deps/clipped-689ce660451a9e6f.d: src/main.rs
-
-src/main.rs:
