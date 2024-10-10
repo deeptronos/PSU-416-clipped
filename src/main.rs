abd9075348f946cd6354f990d840485d20e8563b
C <colnem@pdx.edu>
2024-10-10 13:41:54 -0700

    Playback

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use hound;
 use rodio::{source::Source, Decoder, OutputStream};
 use std::f32::consts::PI;
 use std::fs::File;
@@ -32,7 +31,7 @@ fn write_clipped() {
     let mut writer = hound::WavWriter::create("clipped.wav", spec).unwrap();
     for t in (0..48000).map(|x| x as f32 / 48000.0) {
         let sample = (t * 440.0 * 2.0 * PI).sin();
-        let amplitude = ((i16::MAX as f32) / 2.0);
+        let amplitude = (i16::MAX as f32) / 2.0;
         writer
             .write_sample(
                 (sample * amplitude).clamp((i16::MIN as f32) / 4.0, (i16::MAX as f32) / 4.0) as i16,
@@ -48,7 +47,7 @@ fn play_clipped() {
     let source = Decoder::new(file).unwrap();
     stream_handle
         .play_raw(source.convert_samples())
-        .expect("Error while playing raw");
+        .expect("Error during play_raw");
 
     // Keep main thread alive while seperate audio thread plays.
     std::thread::sleep(std::time::Duration::from_secs(5));
